79fe2e21c1f27cad4ce4ae1dc8227939b7e5fe04
Inna Mihailova <innna.p.m@gmail.com>
2013-07-07 18:21:52 +0300

    bugfix: post tags

--- db/migrate/20130706185119_create_posts.rb
+++ db/migrate/20130706185119_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :text
-      t.string :tags, array: true, default: '{}'
+      t.text :tags, array: true
       t.boolean :private
 
       t.timestamps
